7a3c4e0d3949b659b4628365013447b6e6b70b21
Adriaan Knapen <hi@aknapen.nl>
2025-04-24 15:37:41 +0200

    test: configure Cypress to test for mobile screen sizes

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  // Default viewport for testing on mobile
+  viewportWidth: 375,
+  viewportHeight: 667,
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
